16e165a41a5872a32901bf2bab1a083edc79f774
Miguigraming987 <1916772@alu.murciaeduca.es>
2024-12-17 23:26:03 +0100

    creacion de main y operadores

--- calculadora1.java
+++ calculadora1.java
@@ -1,4 +1,9 @@
 public class calculadora1 {
-
-    
+    public static void main(String[] args) {
+        // Valores predefinidos (puedes cambiarlos aquí)
+        double num1 = 10.5;
+        char operador = '+';
+        double num2 = 3.2;
+        }
+        
 }
